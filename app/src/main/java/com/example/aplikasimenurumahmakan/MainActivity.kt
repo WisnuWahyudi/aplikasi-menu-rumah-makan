91e7a8898e63a5e3bc0909baa0ea87ec4c97c41e
Zulka <zulka.ajuab@gmail.com>
2020-09-11 15:47:52 +0700

    Perubahan nama pada button bayar dan button hitung

--- app/src/main/java/com/example/aplikasimenurumahmakan/MainActivity.kt
+++ app/src/main/java/com/example/aplikasimenurumahmakan/MainActivity.kt
@@ -4,8 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
-import android.widget.CheckBox
-import android.widget.TextView
+
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
